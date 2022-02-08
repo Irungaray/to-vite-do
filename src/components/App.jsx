d3963f161e0afb97d463b523cceb614879186393
Irungaray <LionelIrungaray@GMail.com>
2022-02-08 10:18:06 -0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+const App = (props) => {
+  return (
+    <>
+      <p>Hello Vite + Preact!</p>
+      <p>
+        <a
+          class="link"
+          href="https://preactjs.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Preact
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default App;
